03755cda2c23ebfcd1acb4f2f667dfe8e322ae8a
Andrews Medina <andrewsmedina@gmail.com>
2011-08-26 00:59:56 -0300

    added y attribute in sprite

--- physics/sprite.py
+++ physics/sprite.py
@@ -1,4 +1,5 @@
 class Sprite(object):
 
-    def __init__(self, x):
+    def __init__(self, x, y):
         self.x = x
+        self.y = y
--- tests/test_sprite.py
+++ tests/test_sprite.py
@@ -5,6 +5,11 @@ from physics.sprite import Sprite
 
 class SpriteTestCase(TestCase):
 
+    def setUp(self):
+        self.sprite = Sprite(x=10, y=15)
+
     def test_sprite_should_have_x_attribute(self):
-        sprite = Sprite(x=10)
-        self.assertEqual(10, sprite.x)
+        self.assertEqual(10, self.sprite.x)
+
+    def test_sprite_should_have_y_attribute(self):
+        self.assertEqual(15, self.sprite.y)
