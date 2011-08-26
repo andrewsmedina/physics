daff32b2fc66d7b3f6672c117870dbfe8e48e926
Andrews Medina <andrewsmedina@gmail.com>
2011-08-26 01:14:08 -0300

    refatored sprite moving velocity attributes into sprite instance

--- physics/sprite.py
+++ physics/sprite.py
@@ -1,9 +1,11 @@
 class Sprite(object):
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, x_velocity=0, y_velocity=0):
         self.x = x
         self.y = y
+        self.x_velocity = x_velocity
+        self.y_velocity = y_velocity
 
-    def step(self, x_velocity=0, y_velocity=0):
-        self.x += x_velocity
-        self.y += y_velocity
+    def step(self, ):
+        self.x += self.x_velocity
+        self.y += self.y_velocity
--- tests/test_sprite.py
+++ tests/test_sprite.py
@@ -6,7 +6,7 @@ from physics.sprite import Sprite
 class SpriteTestCase(TestCase):
 
     def setUp(self):
-        self.sprite = Sprite(x=10, y=15)
+        self.sprite = Sprite(x=10, y=15, x_velocity=1, y_velocity=1)
 
     def test_sprite_should_have_x_attribute(self):
         self.assertEqual(10, self.sprite.x)
@@ -15,9 +15,9 @@ class SpriteTestCase(TestCase):
         self.assertEqual(15, self.sprite.y)
 
     def test_step_should_increment_x_sprite_position(self):
-        self.sprite.step(x_velocity=1)
+        self.sprite.step()
         self.assertEqual(11, self.sprite.x)
 
     def test_step_should_increment_y_sprite_position(self):
-        self.sprite.step(y_velocity=1)
+        self.sprite.step()
         self.assertEqual(16, self.sprite.y)
